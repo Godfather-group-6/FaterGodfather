844bde9931e0326b76d31f8b85d21f1ae2e247e2
Antoine Jeanselme <jeanselme.antoine@gmail.com>
2020-09-03 13:25:48 +0200

    Adding sad charac base interaction scripts
    + help counter

---
+++ Assets/Scripts/Antoine/HelpManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpManager : MonoBehaviour
+{
+    public static HelpManager instance;
+
+    public int characteresHelped = 0;
+
+
+    void Awake(){
+        instance = this;
+    }
+
+    public void personHelped() 
+    {
+        characteresHelped++;
+    }
+}
